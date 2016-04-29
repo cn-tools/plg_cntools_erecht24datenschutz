08de9f018d72fc78c4ec8acb2d7fe16062b2357e
cn-tools <cn-tools@gmx.at>
2016-04-29 06:31:22 +0200

    add acknowledge, youtube, ...

--- plg_cntools_erecht24datenschutz.php
+++ plg_cntools_erecht24datenschutz.php
@@ -34,10 +34,15 @@ class plgContentPlg_CNTools_ERecht24Datenschutz extends JPlugin{
 	}
 	//------------------------------------------------------------------------
 	function render(&$matches){
-		$lValue = htmlspecialchars_decode($matches[0]);
-		$lValue = substr($lValue, 21);
-		$lValue = substr($lValue, 0, strlen($lValue)-22);
-		$lResult = $this->addContent($lValue);
+		if ($this->params->get('plg_cntools_e24d_acknowledge', '0') != '1')
+		{
+			$lResult = "<p><strong>Bitte bestätigen Sie zuerst in den Einstellungen des Plug-In's, dass Sie die Hinweise zum Datenschutzgenerator gelesen und akzeptiert haben.</strong></p>";
+		} else {
+			$lValue = htmlspecialchars_decode($matches[0]);
+			$lValue = substr($lValue, 21);
+			$lValue = substr($lValue, 0, strlen($lValue)-22);
+			$lResult = $this->addContent($lValue);
+		}
 
 		return $lResult;
 	}
@@ -64,7 +69,7 @@ class plgContentPlg_CNTools_ERecht24Datenschutz extends JPlugin{
 			$http = JHttpFactory::getHttp(); 
 			try
 			{
-				$response = $http->get('http://www.e-recht24.de/plugins/content/disclaimermaker/assets/dmaker.php?'.$phrase, null, 6);
+				$response = $http->get('http://www.e-recht24.de/plugins/content/disclaimermaker/assets/dmaker.php?acknowledge='.$this->params->get('plg_cntools_e24d_acknowledge', '0').$phrase, null, 6);
 				
 				$stringJSONFull = $response->body; 
 			}
@@ -81,30 +86,33 @@ class plgContentPlg_CNTools_ERecht24Datenschutz extends JPlugin{
 				
 				$lResult .= $this->GetRueckfallText($phrase, 'standard');
 				$lResult .= $this->GetRueckfallText($phrase, 'privacy');
-				$lResult .= $this->GetRueckfallText($phrase, 'facebook');
+				$lResult .= $this->GetRueckfallText($phrase, 'cookies');
+				$lResult .= $this->GetRueckfallText($phrase, 'serverlogfiles');
+				$lResult .= $this->GetRueckfallText($phrase, 'contactform');
+				$lResult .= $this->GetRueckfallText($phrase, 'newsletter');
 				$lResult .= $this->GetRueckfallText($phrase, 'analytics');
-				$lResult .= $this->GetRueckfallText($phrase, 'plusone');
-				//$lResult .= $this->GetRueckfallText($phrase, 'adsense');
-				//$lResult .= $this->GetRueckfallText($phrase, 'remarketing');
 				$lResult .= $this->GetRueckfallText($phrase, 'etracker');
+				$lResult .= $this->GetRueckfallText($phrase, 'piwik');
+				$lResult .= $this->GetRueckfallText($phrase, 'facebook');
 				$lResult .= $this->GetRueckfallText($phrase, 'twitter');
-				$lResult .= $this->GetRueckfallText($phrase, 'xing');
+				$lResult .= $this->GetRueckfallText($phrase, 'plusone');
 				$lResult .= $this->GetRueckfallText($phrase, 'instagram');
 				$lResult .= $this->GetRueckfallText($phrase, 'linkedin');
 				$lResult .= $this->GetRueckfallText($phrase, 'pinterest');
+				$lResult .= $this->GetRueckfallText($phrase, 'xing');
 				$lResult .= $this->GetRueckfallText($phrase, 'tumblr');
+				$lResult .= $this->GetRueckfallText($phrase, 'youtube');
+				//$lResult .= $this->GetRueckfallText($phrase, 'adsense');
+				//$lResult .= $this->GetRueckfallText($phrase, 'remarketing');
 				//$lResult .= $this->GetRueckfallText($phrase, 'amazon');
-				$lResult .= $this->GetRueckfallText($phrase, 'infodelete');
-				$lResult .= $this->GetRueckfallText($phrase, 'serverlogfiles');
-				$lResult .= $this->GetRueckfallText($phrase, 'cookies');
-				$lResult .= $this->GetRueckfallText($phrase, 'contactform');
-				$lResult .= $this->GetRueckfallText($phrase, 'advertemail');
-				$lResult .= $this->GetRueckfallText($phrase, 'newsletter');
 				//$lResult .= $this->GetRueckfallText($phrase, 'registration');
+				//$lResult .= $this->GetRueckfallText($phrase, 'dataprocessing');
 				//$lResult .= $this->GetRueckfallText($phrase, 'shops');
 				//$lResult .= $this->GetRueckfallText($phrase, 'onlinecontracts');
-				//$lResult .= $this->GetRueckfallText($phrase, 'dataprocessing');
-				
+				$lResult .= $this->GetRueckfallText($phrase, 'infodelete');
+				$lResult .= $this->GetRueckfallText($phrase, 'advertemails');
+				//$lResult .= $this->GetRueckfallText($phrase, 'translation_en');
+
 				$lTxt = trim($this->params->get('plg_cntools_e24d_fallback_bottom'));
 				if ($lTxt<>''){
 					$lResult .= '<p>'.$lTxt.'</p>';
@@ -112,7 +120,7 @@ class plgContentPlg_CNTools_ERecht24Datenschutz extends JPlugin{
 				
 				$lResult = trim($lResult);
 				if ($lResult == '') {
-					$lResult = '<p>Der Haftungsausschluss und/oder die Datenschutzerklärung von <a target="_blank" href="http://www.e-recht24.de">www.e-recht24.de</a> steht derzeit nicht zur Verfügung!</p><p>Sollte dieses Problem länger bestehen, kontaktieren Sie bitte den Betreiber dieser Homepage!</p>';
+					$lResult = '<p>Der Haftungsausschluss und/oder die Datenschutzerklärung von <a target="_blank" href="http://www.e-recht24.de">www.e-recht24.de</a> steht derzeit nicht zur Verfügung!<br />Sollte dieses Problem länger bestehen, kontaktieren Sie bitte den Betreiber dieser Homepage!</p>';
 				}
 
 				$lErrorEmail = $this->params->get('plg_cntools_e24d_erroremail');
